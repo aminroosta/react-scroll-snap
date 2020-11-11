26357c12f6e51dba758252989218e3831e1d8940
amin roosta <amin.roosta@outlook.com>
2020-11-11 15:58:14 +0800

    first version

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-scroll-snap'
+import { SnapY } from 'react-scroll-snap'
 import 'react-scroll-snap/dist/index.css'
 
+const page_stl = {
+  width: '100%',
+  height: '100%',
+};
+
+let keyframes = [
+  { background: 'slateblue' },
+  { background: 'teal' },
+  { background: 'tomato' },
+  { background: 'skyblue' },
+];
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <SnapY keyframes={keyframes}>
+    <div style={{ ...page_stl }} >page 1</div>
+    <div style={{ ...page_stl }}>page 2</div>
+    <div style={{ ...page_stl }}>page 3</div>
+    <div style={{ ...page_stl }}>page 4</div>
+  </SnapY>
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,76 @@
 import * as React from 'react'
 import styles from './styles.module.css'
 
-interface Props {
-  text: string
+// @ts-ignore
+const { useRef, useState, useEffect } = React;
+
+type Page = React.FC<{
+  frames?: Keyframe[],
+  children: React.ReactNode
+}>;
+
+export const Page: Page = ({ children, frames = [] }) => {
+  return <div>{children}</div>
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+type SnapY = React.FC<{
+  children?: React.ReactNode,
+  keyframes?: Keyframe[]
+}>;
+export const SnapY: SnapY = ({ children, keyframes = [] }) => {
+  // @ts-ignore
+  let root = useRef(null as HTMLDivElement);
+  let refs: React.MutableRefObject<HTMLElement>[] = [];
+  let count = React.Children.count(children);
+
+  let pages = React.Children.map(children, child => {
+    // @ts-ignore
+    const ref = useRef(null);
+    // @ts-ignore
+    refs.push(ref);
+    // @ts-ignore
+    return React.cloneElement(child, { ref });
+  });
+
+
+  function on_scroll(animations: Animation[]) {
+    const scroll = root.current.scrollTop / (root.current.scrollHeight - root.current.offsetHeight) * (count - 1);
+
+    animations.forEach((a, idx) => {
+      let progress = scroll - Math.max(0, idx - 1);
+      progress = Math.max(0, Math.min(progress, 2));
+      a.currentTime = progress * 1000;
+    });
+  };
+
+  useEffect(() => {
+    const animations = [] as Animation[]
+    const callback = () => on_scroll(animations);
+    root.current.addEventListener('scroll', callback, { passive: true });
+
+    refs.forEach((r, idx) => {
+      // @ts-ignore
+      r.current.style.scrollSnapAlign = 'start';
+
+      if (idx >= keyframes.length) {
+        return;
+      }
+
+      let from = Math.max(idx - 1, 0);
+      let to = Math.min(idx + 2, count);
+      let frames = keyframes.slice(from, to);
+      let duration = (frames.length - 1) * 1000;
+      let animation = r.current.animate(
+        frames, { iterations: 1, fill: 'both', duration }
+      );
+      animation.pause();
+      animations.push(animation);
+    });
+
+    return () => root.current.removeEventListener('scroll', callback);
+  }, []);
+
+  return <div ref={root} className={styles.snap_y}>
+    {pages}
+  </div>
 }
